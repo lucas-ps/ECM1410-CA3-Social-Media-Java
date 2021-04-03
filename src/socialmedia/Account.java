9740a8ea7eef750061ba89abdf9b2dc8e1b77006
umennear <75434681+umennear@users.noreply.github.com>
2021-04-03 14:34:07 +0100

    Setting up classes

---
+++ src/socialmedia/Account.java
@@ -0,0 +1,11 @@
+package socialmedia;
+
+public class Account{
+    // constraints (handle must be unique) (ID must be created whilst the account is created and must be unique)
+    //methods (constructor to create account) (store accounts and be able to delete them given id)
+    private String description;
+    private int id;
+    private String Handle;
+
+
+}
