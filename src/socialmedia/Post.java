58c89ee7377a4417b9fdc24b281c3db23a6d8694
umennear <75434681+umennear@users.noreply.github.com>
2021-04-03 14:50:30 +0100

    Setting up classes

--- src/socialmedia/Post.java
+++ src/socialmedia/Post.java
@@ -1,5 +1,11 @@
 package socialmedia;
 
-public class Post{
+public class Post {
+    //Constraints (postId goes up in chronological order) (100 character limit on contents)
+    //Methods (constructor to create posts posts) (remove from list using postId)
+
+    private int postId;
+    private String contents;
+    private Account author;
 
 }
