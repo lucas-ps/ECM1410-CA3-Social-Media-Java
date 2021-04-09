f33a7670e37572c8c6b710a23de74f5a1d9ff6b3
Lucas Proudhon Smith <75811985+lucas-ps@users.noreply.github.com>
2021-04-09 15:33:00 +0100

    Created endorsement constructor

--- src/socialmedia/Endorsement.java
+++ src/socialmedia/Endorsement.java
@@ -1,5 +1,12 @@
 package socialmedia;
 
 public class Endorsement extends Post{
+    private Original parent;
+    private PostType type;
 
+    public Endorsement(Account author, String contents, Original parent) throws InvalidPostException {
+        super(author, contents);
+        this.parent = parent;
+        this.type = PostType.ENDORSEMENT;
+    }
 }
