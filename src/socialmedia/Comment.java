aa15863987776ef6b237339c53ecc822c4c8b43f
Lucas Proudhon Smith <75811985+lucas-ps@users.noreply.github.com>
2021-04-09 15:32:28 +0100

    Added comment post constructor

--- src/socialmedia/Comment.java
+++ src/socialmedia/Comment.java
@@ -1,10 +1,16 @@
 package socialmedia;
 
 public class Comment extends Post{
-    //ToDo
-    private int originalId;
-    private Account author;
-    private String contents;
-    private int postId;
+    private boolean isOrphan;
+    private Post parent;
+    private PostType type;
 
+    public Comment(Account author, String contents, boolean isOrphan, Post parent)
+            throws InvalidPostException {
+        super(author, contents);
+        this.isOrphan = isOrphan;
+        this.type = PostType.COMMENT;
+        this.parent = parent;
+
+    }
 }
