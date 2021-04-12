ffe8542237ff782e7e5ce3037cb024f7a9ec88a7
Lucas Proudhon Smith <75811985+lucas-ps@users.noreply.github.com>
2021-04-12 22:43:20 +0100

    Added toString method for comments, added documentation

--- src/socialmedia/Comment.java
+++ src/socialmedia/Comment.java
@@ -16,6 +16,20 @@ public class Comment extends Post{
         this.isOrphan = isOrphan;
         this.type = PostType.COMMENT;
         this.parent = parent;
+    }
 
+    // TODO: Add in indent to comments
+    /**
+     * The method generates a formated string containing the details of a single
+     * post. The format is as follows:
+     *  ID: [post ID]
+     *  Account: [account handle]
+     *  No. endorsements: [number of endorsements received by the post] | No. comments: [number of comments received by the post]
+     *  [post message]
+     */
+    @Override
+    public String toString(){
+        return "\nId: " + postId + "\nAccount: " + author.getHandle() + "\nNo. endorsements: " +
+                endorsements.size() + " | No. comments: " + comments.size() + "\n" + contents + "\n";
     }
 }
