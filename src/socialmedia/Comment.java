0196ed59f3ca3d10b712c4b6402bd5f69d9e3d02
Lucas Proudhon Smith <75811985+lucas-ps@users.noreply.github.com>
2021-04-12 22:41:01 +0100

    Added new attributes to comment post constructor (originalPost, endorsements, comments. Added exceptions

--- src/socialmedia/Comment.java
+++ src/socialmedia/Comment.java
@@ -3,10 +3,13 @@ package socialmedia;
 public class Comment extends Post{
     private boolean isOrphan;
     private Post parent;
+    private Original originalPost;
     private PostType type;
+    private ArrayList<Endorsement> endorsements;
+    private ArrayList<Comment> comments;
 
     public Comment(Account author, String contents, boolean isOrphan, Post parent)
-            throws InvalidPostException {
+            throws InvalidPostException, HandleNotRecognisedException {
         super(author, contents);
         this.isOrphan = isOrphan;
         this.type = PostType.COMMENT;
