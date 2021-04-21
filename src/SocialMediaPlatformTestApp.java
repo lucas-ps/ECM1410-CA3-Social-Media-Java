b4d19fbfd8c81624152fbbf30e1b14d55896771c
Lucas Proudhon Smith <75811985+lucas-ps@users.noreply.github.com>
2021-04-21 18:01:47 +0100

    Started testing implementation. So far testing has been implemented for: createAccount, showAccount, removeAccount,updateAccountDescription, changeAccountHandle, createPost, endorsePost, commentPost, deletePost,showIndividualPost, savePlatform, loadPlatform, erasePlatform

--- src/SocialMediaPlatformTestApp.java
+++ src/SocialMediaPlatformTestApp.java
@@ -1,8 +1,7 @@
-import socialmedia.AccountIDNotRecognisedException;
-import socialmedia.SocialMedia;
-import socialmedia.IllegalHandleException;
-import socialmedia.InvalidHandleException;
-import socialmedia.SocialMediaPlatform;
+import socialmedia.*;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 /**
  * A short program to illustrate an app testing some minimal functionality of a
@@ -15,6 +14,12 @@ import socialmedia.SocialMediaPlatform;
  * @version 1.0
  */
 public class SocialMediaPlatformTestApp {
+
+	/**
+	 * Print helper method
+	 * @param data the string to be printed to console
+	 */
+	public static void print(String data){System.out.println(data);  }
 
 	/**
 	 * Test method.
@@ -35,22 +40,113 @@ public class SocialMediaPlatformTestApp {
 
 		/**
 		 * Tests for account methods
+		 * createAccount ✓, showAccount ✓, removeAccount ✓, updateAccountDescription ✓, changeAccountHandle ✓,
+		 * getNumberOfAccounts, getMostEndorsedAccount
 		 */
 		try {
+			// Creating an account
 			id = platform.createAccount("my_handle");
+			platform.createAccount("test_1", "testing");
+
+			// Outputting account info
+			String accountInfo = platform.showAccount("my_handle");
+			//print(accountInfo);
+			String accountInfo1 = platform.showAccount("test_1");
+			//print(accountInfo1);
+
+			// Adding/changing account description
+			platform.updateAccountDescription("my_handle", " test description");
+			accountInfo = platform.showAccount("my_handle");
+			//print(accountInfo);
+
+			// Changing an account's handle
+			platform.changeAccountHandle("my_handle", "new_Handle");
+			accountInfo = platform.showAccount("new_Handle");
+			print(accountInfo);
 			assert (platform.getNumberOfAccounts() == 1) : "number of accounts registered in the system does not match";
 
+			// Removing an account
 			platform.removeAccount(id);
+			platform.createAccount("test_2");
 			assert (platform.getNumberOfAccounts() == 0) : "number of accounts registered in the system does not match";
 
+			// TODO: getMostEndorsedAccount, getNumberOfAccounts
 		} catch (IllegalHandleException e) {
 			assert (false) : "IllegalHandleException thrown incorrectly";
 		} catch (InvalidHandleException e) {
 			assert (false) : "InvalidHandleException thrown incorrectly";
 		} catch (AccountIDNotRecognisedException e) {
 			assert (false) : "AccountIDNotRecognizedException thrown incorrectly";
+		} catch (HandleNotRecognisedException e) {
+			assert (false) : "HandleNotRecognisedException thrown incorrectly";
 		}
 
+		/**
+		 * Tests for post methods
+		 * createPost ✓, endorsePost ✓, commentPost ✓, deletePost ✓, showIndividualPost ✓,
+		 * showPostChildrenDetails (+ postHelper), getTotalOriginalPosts, getTotalEndorsmentPosts, getTotalCommentPosts,
+		 * getMostEndorsedPost
+		 * // TODO: Endorsement count for posts not working
+		 */
+		try {
+			// createPost, showIndividualPost
+			platform.createPost("test_1", "hello world");
+			print(platform.showIndividualPost(1));
+			print(platform.showAccount("test_1"));
+
+			// endorsePost
+			platform.endorsePost("test_2", 1);
+			print(platform.showIndividualPost(2));
+			print(platform.showIndividualPost(1));
+			print(platform.showAccount("test_2"));
+
+			// commentPost
+			platform.commentPost("test_2", 1, "hi");
+			print(platform.showIndividualPost(3));
+			print(platform.showIndividualPost(1));
+
+			// showPostChildrenDetails
+			platform.showPostChildrenDetails(1);
+
+			// deletePost
+			platform.deletePost(1);
+			try {
+				print(platform.showAccount("test_1"));
+			} catch (Exception e) {
+				print("Post successfully removed");
+			}
+
+		} catch(PostIDNotRecognisedException e) {
+			assert (false) : "PostIDNotRecognisedException thrown incorrectly";
+		}  catch(InvalidPostException e) {
+			assert (false) : "InvalidPostException thrown incorrectly";
+		} catch (HandleNotRecognisedException e) {
+			assert (false) : "HandleNotRecognisedException thrown incorrectly";
+		}catch (NotActionablePostException e) {
+			assert (false) : "NotActionablePostException thrown incorrectly";
+			assert (true) : "NotActionablePostException";
+		}
+
+		/**
+		 * Tests for platform methods
+		 * savePlatform ✓, loadPlatform ✓, erasePlatform ✓
+         */
+		try {
+			platform.savePlatform("testSave");
+			// Saving hashmaps locally so that they can be used to see if loadPlatform() works
+			HashMap<Integer, Post> localPosts = ((SocialMedia) platform).posts;
+			HashMap<String, Account> localAccounts = ((SocialMedia) platform).accounts;
+
+			platform.erasePlatform();
+
+			platform.loadPlatform("testSave");
+			assert ((SocialMedia) platform).accounts.equals(localAccounts) : "Accounts not loaded correctly";
+			assert ((SocialMedia) platform).posts.equals(localPosts) : "Posts not loaded correctly";
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
